011c7cab4fa10e02b463ac0622de3ef1c32c37f8
Sh031224 <1cktmdgh2@gmail.com>
2020-09-14 12:28:20 +0900

    [ADD] styled-component

--- src/lib/createFile.ts
+++ src/lib/createFile.ts
@@ -1,4 +1,4 @@
-import { workspace, Uri, window } from "vscode";
+import { workspace, Uri } from "vscode";
 import * as fs from "fs";
 import * as fse from "fs-extra";
 import * as path from "path";
@@ -27,7 +27,7 @@ export const createComponent = (
   const globalConfig: GlobalConfig = getConfig().get("global");
   const componentConfig: ComponentConfig = getConfig().get("componentFile");
   const styleConfig: StyleConfig = getConfig().get("styleFile");
-  const languageName: string = language === "JavaScript" ? "js" : "tsx";
+  const languageName: string = language === "JavaScript" ? "js" : "ts";
   let oSuffix: string = `${suffix}-${componentConfig.type}-${languageName}`;
 
   let templateFileName = assetRootDir + `/template/${oSuffix}.template`;
@@ -39,32 +39,58 @@ export const createComponent = (
     styleContent = `import {quotes}./{componentName}.${styleConfig.type}{quotes}{semi}\n`;
   }
 
-  let componentContent = fs
-    .readFileSync(templateFileName)
-    .toString()
-    .replace(/{style}/g, styleContent)
+  let componentContent = fs.readFileSync(templateFileName).toString();
+
+  if (styleConfig.type === "styled-component") {
+    componentContent = componentContent.replace(
+      /{style}/g,
+      `//import {  } from {quotes}./{componentName}.style.${languageName}{quotes}{semi}`
+    );
+  } else {
+    componentContent = componentContent.replace(/{style}/g, styleContent);
+  }
+  componentContent = componentContent
     .replace(/{componentName}/g, compName)
     .replace(/{quotes}/g, globalConfig.quotes === "double" ? '"' : "'")
     .replace(/{semi}/g, globalConfig.semi ? ";" : "");
 
-  let filename = `${componentDir}/${compName}.${languageName}`;
+  let filename = `${componentDir}/${compName}.${
+    languageName === "js" ? componentConfig.js : "tsx"
+  }`;
 
   createFile(filename, componentContent);
 
   return filename;
 };
 
-export const createStyle = (componentDir: string, componentName: string) => {
+export const createStyle = (
+  componentDir: string,
+  componentName: string,
+  language: string
+) => {
   const styleConfig: StyleConfig = getConfig().get("styleFile");
+  const languageName: string = language === "JavaScript" ? "js" : "ts";
 
   if (styleConfig.create) {
-    let templateFileName = assetRootDir + `/template/style.template`;
+    let templateFileName = assetRootDir + `/template/`;
+
+    if (styleConfig.type === "styled-component") {
+      templateFileName += `css-styled.template`;
+    } else {
+      templateFileName += `style.template`;
+    }
 
     const compName = pascalCase(componentName);
 
     let componentContent = fs.readFileSync(templateFileName).toString();
 
-    let filename = `${componentDir}/${compName}.${styleConfig.type}`;
+    let filename = `${componentDir}/${compName}`;
+
+    if (styleConfig.type === "styled-component") {
+      filename += `.style.${languageName}`;
+    } else {
+      filename += `.${styleConfig.type}`;
+    }
 
     createFile(filename, componentContent);
   } else {
